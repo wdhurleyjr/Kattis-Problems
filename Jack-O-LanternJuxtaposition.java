a180f8a15e760a753fc8817c6f499d75f1b6eb23
wdhurleyjr <128835969+wdhurleyjr@users.noreply.github.com>
2023-03-26 12:27:55 -0700

    Create Jack-O-LanternJuxtaposition.java

---
+++ Jack-O-LanternJuxtaposition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class jackolanternjuxtaposition {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        
+        int input1 = scan.nextInt();
+        int input2 = scan.nextInt();
+        int input3 = scan.nextInt();
+        
+        System.out.print(input1 * input2 * input3);
+    }
+}
